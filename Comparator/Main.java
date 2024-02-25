636a22aee7eb4cfb2de577dc1a1beebcbf3b6789
Ephryll <135094289+MsEphryll@users.noreply.github.com>
2024-02-25 23:49:14 +0800

    Update Main.java
    added an argument for age

--- Comparator/Main.java
+++ Comparator/Main.java
@@ -1,6 +1,6 @@
 public class Main {
   public static void main(String[] args) {
-    Person p = new Person("Bitang", "April", 1.64, 59);
+    Person p = new Person("Bitang", "April", 19, 1.64, 59);
 
     System.out.println(p);
   }
